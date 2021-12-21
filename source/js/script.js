f73f5746b87f69cb059e31922cc0660778ab217d
Daria Liferova <liferova.daria@gmail.com>
2021-12-21 23:03:20 +0100

    Реализация требований ТЗ модального окна.

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,18 @@
 //Реализация маски номера телефона в input
-const phoneField = document.getElementById("question-phone-field");
+const phoneInputs = document.querySelectorAll("input[type=tel]");
 
-const phoneMask = IMask(
-  phoneField, {
-    mask: '+{7}(000)000-00-00'
-  });
+phoneInputs.forEach(input => {
+  const phoneMask = IMask(
+    input, {
+      mask: '+{7}(000)000-00-00'
+    });
 
-phoneField.addEventListener('focus', (evt) => {
-  evt.preventDefault()
-  if (phoneField.value === '') {
-    phoneMask.value = "+7("
-  }
+  input.addEventListener('focus', (evt) => {
+    evt.preventDefault()
+    if (input.value === '') {
+      phoneMask.value = "+7("
+    }
+  })
 })
 
 //Реализация аккордеона
@@ -49,27 +51,65 @@ callToActionLink.addEventListener('click', (evt) => {
 //Реализация поп-апа
 const feedbackButton = document.querySelector('.feedback-button');
 const feedbackFormModal = document.querySelector('.feedback-form_modal');
+const nameInput = feedbackFormModal.querySelector('#name-field');
+const phoneInput = feedbackFormModal.querySelector('#phone-field');
+const questionInput = feedbackFormModal.querySelector('#question-field');
 const feedbackCloseModalButton = document.querySelector('.feedback-form__modal-close-button');
 const modalWrapper = document.querySelector('.modal');
 
+let isModalOpened = false;
+
 const modalClose = () => {
-  modalWrapper.classList.add('modal_closed');
+  isModalOpened = false;
+  modalWrapper.classList.toggle('modal_showed');
   feedbackFormModal.classList.add('feedback-form_modal_close');
 }
 
 const modalOpen = () => {
-  modalWrapper.classList.add('modal_showed');
+  isModalOpened = true;
+  modalWrapper.classList.toggle('modal_showed');
   feedbackFormModal.classList.remove('feedback-form_modal_close');
+  nameInput.focus();
 }
 
-const onEscClose = (evt) => {
+const onKeyPress = (evt) => {
   if (evt.key === 'Escape' || evt.key === 'Esc') {
-    evt.preventDefault();
-    modalClose();
+    if (isModalOpened) {
+      evt.preventDefault();
+      modalClose();
+    }
   }
 }
 
+document.body.addEventListener('click', (evt) => {
+  if (evt.target === modalWrapper) {
+    modalClose();
+  }
+})
+
 feedbackButton.addEventListener('click', modalOpen);
 feedbackCloseModalButton.addEventListener('click', modalClose);
-document.addEventListener('keydown', onEscClose);
-modalWrapper.addEventListener('click', modalClose);
+document.addEventListener('keydown', onKeyPress);
+
+
+// localstorage
+
+const NAME_INPUT_VALUE_STORAGE_KEY = 'feedbackFormNameInputValue';
+const PHONE_INPUT_VALUE_STORAGE_KEY = 'feedbackFormPhoneInputValue';
+const QUESTION_INPUT_VALUE_STORAGE_KEY = 'feedbackFormQuestionInputValue';
+
+nameInput.value = localStorage.getItem(NAME_INPUT_VALUE_STORAGE_KEY)
+phoneInput.value = localStorage.getItem(PHONE_INPUT_VALUE_STORAGE_KEY)
+questionInput.value = localStorage.getItem(QUESTION_INPUT_VALUE_STORAGE_KEY)
+
+nameInput.addEventListener('change', (evt) => {
+  localStorage.setItem(NAME_INPUT_VALUE_STORAGE_KEY, evt.target.value)
+})
+
+phoneInput.addEventListener('change', (evt) => {
+  localStorage.setItem(PHONE_INPUT_VALUE_STORAGE_KEY, evt.target.value)
+})
+
+questionInput.addEventListener('change', (evt) => {
+  localStorage.setItem(QUESTION_INPUT_VALUE_STORAGE_KEY, evt.target.value)
+})
